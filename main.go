c0ff29e003338fe402291d1094d79c9d435397ba
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-03-23 14:14:09 +0545

    added logger and auth  middleware

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
 import (
+	"log"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		apikey := c.GetHeader("X-API-KEY")
+		if apikey == "" {
+			c.AbortWithStatusJSON(401, gin.H{"error": "API key is missing"})
+			return
+		}
+		c.Next()
+	}
+}
+
+func LoggerMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+		duration := time.Since(start)
+		log.Printf("Request - Method: %s | Status: %d | Duration: %v", c.Request.Method, c.Writer.Status(), duration)
+	}
+}
 
 func main() {
 	// Create a default Gin router
@@ -12,6 +35,15 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello World")
 	})
+
+	// AuthMiddleware for specific routes
+	authGroup := router.Group("/api")
+	authGroup.Use(AuthMiddleware())
+	{
+		authGroup.GET("/data", func(c *gin.Context) {
+			c.JSON(200, gin.H{"message": "Authenticated and authorized!"})
+		})
+	}
 
 	// Run the server on port 8080
 	router.Run(":8080")
