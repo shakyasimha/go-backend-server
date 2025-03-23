1bb4348a821003600a3283a0195b4200065033f3
shakyasimha <sushakya235@gmail.com>
2025-03-23 09:06:26 +0545

    writing gin apis

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// Create a default Gin router
+	router := gin.Default()
+
+	// Define a route for the root URL
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello World")
+	})
+
+	// Run the server on port 8080
+	router.Run(":8080")
 }
